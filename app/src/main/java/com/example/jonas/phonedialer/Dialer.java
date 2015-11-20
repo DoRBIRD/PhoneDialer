6d131ab3aaa23fe56c681ee45eadc267eb17b5a0
DoRBIRD <jonas@oja.de>
2015-11-20 18:46:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/jonas/phonedialer/Dialer.java
@@ -0,0 +1,13 @@
+package com.example.jonas.phonedialer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dialer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialer);
+    }
+}
